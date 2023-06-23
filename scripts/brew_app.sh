39b4e6f61b9ec59141b32fac3be3833f93c79c30
Naoya Tatetsu <60126978+NaoyaTatetsu@users.noreply.github.com>
2023-06-23 10:36:29 +0900

    add rectongle (#5)

--- scripts/brew_app.sh
+++ scripts/brew_app.sh
@@ -2,17 +2,19 @@
 
 sudo -v
 
+brew install asdf
+brew install tree
+brew install jq
+
+brew install --cask docker
 brew install --cask google-chrome
-brew install --cask visual-studio-code
+brew install --cask google-japanese-ime
 brew install --cask iterm2
 brew install --cask karabiner-elements
-brew install --cask notion
-brew install --cask zoom
-brew install --cask google-japanese-ime
-brew install --cask slack
-brew install --cask docker
-brew install tree
-brew install asdf
-brew install --cask mongodb-compass
-brew install jq
 brew install --cask meetingbar
+brew install --cask mongodb-compass
+brew install --cask notion
+brew install --cask rectangle
+brew install --cask slack
+brew install --cask visual-studio-code
+brew install --cask zoom
