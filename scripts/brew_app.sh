c7b077c923bb457b9376be5a4f4c87bc0518bd22
Naoya Tatetsu <60126978+NaoyaTatetsu@users.noreply.github.com>
2023-06-28 11:11:52 +0900

    microsoft edgeとgoogle drive streamを追加 (#6)

--- scripts/brew_app.sh
+++ scripts/brew_app.sh
@@ -8,10 +8,12 @@ brew install jq
 
 brew install --cask docker
 brew install --cask google-chrome
+brew install --cask google-drive
 brew install --cask google-japanese-ime
 brew install --cask iterm2
 brew install --cask karabiner-elements
 brew install --cask meetingbar
+brew install --cask microsoft-edge
 brew install --cask mongodb-compass
 brew install --cask notion
 brew install --cask rectangle
