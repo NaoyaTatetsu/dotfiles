5b7cfa60fb13059ab90a8927e745a51d0993e1c2
NaoyaTatetsu <tttnaoya@p-jihyo.jp>
2024-09-29 00:38:36 +0900

    add: keyboard.sh

---
+++ systems/keyboard.sh
@@ -0,0 +1,16 @@
+#!/bin/shs
+
+# debug log
+set -e
+
+# 管理者パスワードを前もって聞いておく
+sudo -v
+
+# 表示件数を8x10にする
+defaults write com.apple.dock springboard-rows -int 8
+defaults write com.apple.dock springboard-columns -int 10
+# Reset LaunchPad (but not regular)
+defaults write com.apple.dock ResetLaunchPad -bool true
+
+# 設定の反映
+killall Dock
