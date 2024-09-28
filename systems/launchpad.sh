272db1b731fb30a43007347e98f3c9d049e684a2
NaoyaTatetsu <tttnaoya@p-jihyo.jp>
2024-09-29 00:35:12 +0900

    add: launchpad.sh

---
+++ systems/launchpad.sh
@@ -0,0 +1,13 @@
+#!/bin/shs
+
+# debug log
+set -e
+
+# 管理者パスワードを前もって聞いておく
+sudo -v# 表示件数を8x10にする
+defaults write com.apple.dock springboard-rows -int 8
+defaults write com.apple.dock springboard-columns -int 10
+# Reset LaunchPad (but not regular)
+defaults write com.apple.dock ResetLaunchPad -bool true
+
+killall Dock
